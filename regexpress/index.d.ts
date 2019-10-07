36034fff7601643cbcf6f83c36ae4b8ecdbee09e
Geert-Jan Zwiers <geertjanzwiers@gmail.com>
2019-10-07 20:53:21 +0200

    bugfix

---
+++ regexpress/index.d.ts
@@ -0,0 +1,24 @@
+export interface RegexSettings {
+    template: string;
+    flags: string;
+}
+export interface RegexData {
+    settings: RegexSettings;
+    [key: string]: any;
+}
+export interface PlaceholderSubstitutes {
+    [key: string]: any;
+}
+export declare class RegexBuilder {
+    private _regexData;
+    private _placeholderSubstitutes;
+    constructor(placeholderSubstitutes: PlaceholderSubstitutes);
+    setPlaceholderSubstitutes(subs: PlaceholderSubstitutes): void;
+    buildRegex(regexData: RegexData): RegExp;
+    private _buildValues;
+    private _joinArrayWithPipeSymbols;
+    private _buildTemplate;
+    private _substituteTemplateVariables;
+    private _substitutePlaceholders;
+    private _mapCaptureGroups;
+}
--- regexpress/index.js
+++ regexpress/index.js
@@ -1,101 +1,64 @@
-export interface RegexSettings
-{
-    template: string,
-    flags: string
-}
-
-export interface RegexData
-{
-    settings: RegexSettings
-    [key: string]: any
-}
-
-export interface PlaceholderSubstitutes
-{
-    [key: string]: any
-}
-
-export class RegexBuilder
-{
-    private _regexData: RegexData;
-    private _placeholderSubstitutes: PlaceholderSubstitutes;
-
-    constructor(placeholderSubstitutes: PlaceholderSubstitutes) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class RegexBuilder {
+    constructor(placeholderSubstitutes) {
         this._placeholderSubstitutes = placeholderSubstitutes;
-        this._regexData = { settings: { template: "empty", flags: ""}, "empty": ['empty']};
+        this._regexData = { settings: { template: "empty", flags: "" }, "empty": ['empty'] };
     }
-
-    public setPlaceholderSubstitutes(subs: PlaceholderSubstitutes) {
+    setPlaceholderSubstitutes(subs) {
         this._placeholderSubstitutes = subs;
     }
-
-    public buildRegex(regexData: RegexData) {
+    buildRegex(regexData) {
         this._regexData = regexData;
-
         this._buildValues();
-
         return new RegExp(this._buildTemplate(), this._regexData.settings.flags);
     }
-
-    private _buildValues(){
-        for (let key in this._regexData){
+    _buildValues() {
+        for (let key in this._regexData) {
             if (!Array.isArray(this._regexData[key]))
                 continue;
-        
             this._regexData[key] = this._joinArrayWithPipeSymbols(this._regexData[key]);
         }
-
     }
-
-    private _joinArrayWithPipeSymbols(arr: Array<string>) : string {
+    _joinArrayWithPipeSymbols(arr) {
         return arr.join('|');
     }
-
-    private _buildTemplate() : string {
+    _buildTemplate() {
         if (!this._placeholderSubstitutes)
             return this._substituteTemplateVariables(this._regexData.settings.template);
-
         return this._substitutePlaceholders(this._substituteTemplateVariables(this._regexData.settings.template));
     }
-
-    private _substituteTemplateVariables(template: string) : string {
+    _substituteTemplateVariables(template) {
         const template_vars = template.match(/\w+/g);
-
-        if (template_vars === null) throw new Error('Regexpress: cannot build regex with 0 template groups');
-
+        if (template_vars === null)
+            throw new Error('Regexpress: cannot build regex with 0 template groups');
         for (let template_var of template_vars) {
             if (!this._regexData[template_var]) {
                 continue;
             }
-
             template = template.replace(template_var, this._regexData[template_var]);
         }
-
         return template;
     }
-
-    private _substitutePlaceholders(regexString: string) : string {
+    _substitutePlaceholders(regexString) {
         for (let name in this._placeholderSubstitutes) {
             let substitute = this._placeholderSubstitutes[name];
             regexString = regexString.replace(`~~${name}~~`, substitute.join('|'));
         }
-
         return regexString;
     }
-
-    private _mapCaptureGroups(template: string) : Array<string> {
+    _mapCaptureGroups(template) {
         const groups = template.match(/\((\w+?)\)/g);
         let map = [];
         map[0] = 'full_match';
-
-        if (groups === null) return map;
-  
-        for (let i = 0; i < groups.length; i++){
+        if (groups === null)
+            return map;
+        for (let i = 0; i < groups.length; i++) {
             let key = groups[i][1];
             map[i + 1] = key;
         }
-  
         return map;
     }
-
 }
+exports.RegexBuilder = RegexBuilder;
+//# sourceMappingURL=index.js.map
