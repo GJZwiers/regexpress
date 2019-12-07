29dd09cc24746709cb1f9969bcab5c9b570957e5
Geert-Jan Zwiers <geertjanzwiers@gmail.com>
2019-12-07 17:56:31 +0100

    typescript type issue fix

--- src/templatebuilder.ts
+++ src/templatebuilder.ts
@@ -1,36 +1,51 @@
 import { AugmentedExp } from './augmentedExp';
 import { RegexBuilderBase } from './builderbase';
+import { RXListSettings, RXSettings, RXData, RXPlaceholder } from './IRegex';
 
 interface Buildable {
     build() : RegExp;
 }
 
 export class TemplateBuilder extends RegexBuilderBase {
+    protected _settings: RXSettings;
+
+    constructor(regexData: RXData, settings: RXSettings, placeholders?: RXPlaceholder) {
+        super(regexData, settings, placeholders);
+        this._settings = settings;  
+    }
 
     public build() : AugmentedExp {
         return this._buildRegex(this._buildTemplate(this._settings.template));
     }
 
-    private _buildRegex(template: string) : AugmentedExp {
-        return new AugmentedExp(template, this._settings.flags, this._settings.template);
+    private _buildRegex(regexString: string) : AugmentedExp {
+        return new AugmentedExp(regexString, this._settings.flags, this._settings.template);
     }
 
 }
 
 export class TemplateListBuilder extends RegexBuilderBase {
+    protected _settings: RXListSettings;
 
-    public build() : Array<RegExp> {
+    constructor(regexData: RXData, settings: RXListSettings, placeholders?: RXPlaceholder) {
+        super(regexData, settings, placeholders);
+        this._settings = settings;
+    }
+
+    public build() : Array<AugmentedExp> {
         if (!this._settings.templateList) throw new Error('templateList must be defined');
-        const regexList: Array<RegExp> = [];
+        const regexList: Array<AugmentedExp> = [];
         for (let template of this._settings.templateList) {
-            regexList.push(this._buildRegex(this._buildTemplate(template)));
+            const regexString = this._buildTemplate(template);
+            regexList.push(this._buildRegex(regexString, template));
         }
         
         return regexList;
     }
 
-    private _buildRegex(template: string) : RegExp {
-        return new RegExp(template, this._settings.flags);
+    private _buildRegex(regexString: string, template: string) : AugmentedExp {
+        if (!this._settings.templateList) throw new Error('templateList must be defined');
+        return new AugmentedExp(regexString, this._settings.flags, template);
     }
 
 }
