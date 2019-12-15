a6bcd64154de14333e9dc309c502291d5707597f
Geert-Jan Zwiers <geertjanzwiers@gmail.com>
2019-12-15 16:00:40 +0100

    refactoring methods

--- src/builderbase.ts
+++ src/builderbase.ts
@@ -3,9 +3,9 @@ import { TemplateMapper } from './templatemapper';
 
 // perform sorting on regex data before build methods?
 
-interface LogicObj {
-    pattern: RegExp,
-    fn: any
+interface RegexDto {
+    quant: RegExp,
+    nonQuant: RegExp
 }
 
 export abstract class RegexBuilderBase {
@@ -45,7 +45,8 @@ export abstract class RegexBuilderBase {
     public autoSort() {
         for (const namedGroup in this._regexData) {
             if (this._isAutoSortable(this._regexData[namedGroup])) {
-                this._sortGroup(<string[]> this._regexData[namedGroup]);
+                const sortedGroup = this._sortGroup(<string[]> this._regexData[namedGroup]);
+                console.log(sortedGroup);
             }
         }
     }
@@ -57,71 +58,47 @@ export abstract class RegexBuilderBase {
     }
 
     protected _sortGroup(group: string[]) : string[] {
+        const regexes: RegexDto = {
+            quant: /.\{(\d+),?\s?(\d+)?\}/g,
+            nonQuant: /(?!<=\{)\w+(?!\{|.+\}[}{*+])|(?=\\)[?+*$^{}()\[\]\\]/g
+        };
+
+        const sortLogic = (a: string, b: string) => {
+            return this._getLargestMatchLength(b, regexes) - this._getLargestMatchLength(a, regexes);
+        };
+
         const patternsWithMetas: string[] = group.filter(element => {
             return /(?!\\)(?:\*|\+)/.test(element);
         });
+
+        patternsWithMetas.sort(sortLogic);
 
         const patternsWithLiterals: string[] = group.filter(element => {
             return /(?!\\)(?:\*|\+)/.test(element) === false;
         });
 
-        const quantPattern = /.\{(\d+),?\s?(\d+)?\}/g;  // Replace quants?
-        const nonQuantPattern = /(?!<=\{)\w+(?!\{|.+\}[}{*+])|(?=\\)[?+*$^{}()\[\]\\]/g;
-
-        const sortLogic = (a: string, b: string) => {
-            const aMax = this._calculateMatchLength(a, { pattern: quantPattern, fn: this._addQuant }) 
-                        + this._calculateMatchLength(a, { pattern: nonQuantPattern, fn: this._addNonQuant });
-
-            const bMax = this._calculateMatchLength(b, { pattern: quantPattern, fn: this._addQuant })
-                        + this._calculateMatchLength(b, { pattern: nonQuantPattern, fn: this._addNonQuant });
-            
-            return bMax - aMax;
-        };
-
-        patternsWithMetas.sort(sortLogic);
         patternsWithLiterals.sort(sortLogic);
 
-        const allSorted = patternsWithMetas.concat(patternsWithLiterals);
-        console.log(allSorted);
-
-        return allSorted;
+        return patternsWithMetas.concat(patternsWithLiterals);
     }
 
-    protected _calculateMatchLength(regexString: string, obj: LogicObj) : number {
-        const matchesList = regexString.matchAll(obj.pattern);
+    protected _getLargestMatchLength(regexString: string, patterns: RegexDto) : number {
         let total = 0;
 
-        if (matchesList === null)
+        const matches = regexString.replace(patterns.quant, (match, min, max) => {
+            total += (max) ? parseInt(max) : parseInt(min);
+            return '';
+        });
+
+        const includeInCount = matches.matchAll(patterns.nonQuant);
+
+        if (includeInCount === null)
             return total;
 
-        for (let matches of matchesList) {
-            total += obj.fn(matches);
-        }
-        console.log(total, regexString);
-        return total;
-    }
-
-    protected _calculateMaxMatchLength(regexString: string) : number {
-        const quantPattern = /.\{(\d+),?\s?(\d+)?\}/g;
-        const nonQuantPattern = /(?!<=\{)\w+(?!\{)|(?=\\)[?+*$^{}()\[\]\\]/g;
-        let total = 0;
-
-        const matches = regexString.matchAll(quantPattern);
-        const includeInCount = regexString.matchAll(nonQuantPattern);
-
-        if (matches !== null) {
-            for (let match of matches) {
-                total += this._addQuant(match);
-            }
+        for (const match of includeInCount) {
+            total += this._addNonQuant(match);
         }
 
-        if (includeInCount !== null) {
-            for (const match of includeInCount) {
-                total += this._addNonQuant(match);
-            }
-        }
-
-        console.log(total, regexString);
         return total;
     }
 
