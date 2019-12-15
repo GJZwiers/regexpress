273417e6e497e0897fd61be80c4f3c7c36f83b72
Geert-Jan Zwiers <geertjanzwiers@gmail.com>
2019-12-15 15:29:36 +0100

    working on autosort

--- src/builderbase.ts
+++ src/builderbase.ts
@@ -3,6 +3,11 @@ import { TemplateMapper } from './templatemapper';
 
 // perform sorting on regex data before build methods?
 
+interface LogicObj {
+    pattern: RegExp,
+    fn: any
+}
+
 export abstract class RegexBuilderBase {
     protected _regexData: RXData;
     protected _settings: RXSettingsBase;
@@ -24,6 +29,19 @@ export abstract class RegexBuilderBase {
         return template;
     }
 
+    protected _buildGroup(group: string[] | string) : string {
+        return (Array.isArray(group)) ? group.join(this._settings.separator || '|') : group;
+    }
+
+    protected _substitutePlaceholder(group: string) : string {
+        const swap = (match: string, p1: string) => {
+            if (!this._placeholderData[p1]) throw new Error(`found undefined placeholder ${match} in regex data`);
+            return this._buildGroup(this._placeholderData[p1]);
+        };
+
+        return group.replace(/~~(\w+)~~/, swap);
+    }
+
     public autoSort() {
         for (const namedGroup in this._regexData) {
             if (this._isAutoSortable(this._regexData[namedGroup])) {
@@ -38,78 +56,83 @@ export abstract class RegexBuilderBase {
                 && this._settings.separator === ('|' || undefined));
     }
 
-    // sort metas descending min length
     protected _sortGroup(group: string[]) : string[] {
-        const metaList: string[] = group.filter((element, index) => {
+        const patternsWithMetas: string[] = group.filter(element => {
             return /(?!\\)(?:\*|\+)/.test(element);
         });
 
-        const litList: string[] = group.filter((element, index) => {
+        const patternsWithLiterals: string[] = group.filter(element => {
             return /(?!\\)(?:\*|\+)/.test(element) === false;
         });
 
-        metaList.sort((a: string, b: string) => {
-            const first = this._calculateMinMatchLength(a);
-            const second = this._calculateMinMatchLength(b);
-            return second - first;
-        });
-        
-        litList.sort((a: string, b: string) => {
-            const first = this._calculateMaxMatchLength(a);
-            const second = this._calculateMaxMatchLength(b);
-            return second - first;
-        });
+        const quantPattern = /.\{(\d+),?\s?(\d+)?\}/g;  // Replace quants?
+        const nonQuantPattern = /(?!<=\{)\w+(?!\{|.+\}[}{*+])|(?=\\)[?+*$^{}()\[\]\\]/g;
 
-        const allSorted = metaList.concat(litList);
+        const sortLogic = (a: string, b: string) => {
+            const aMax = this._calculateMatchLength(a, { pattern: quantPattern, fn: this._addQuant }) 
+                        + this._calculateMatchLength(a, { pattern: nonQuantPattern, fn: this._addNonQuant });
+
+            const bMax = this._calculateMatchLength(b, { pattern: quantPattern, fn: this._addQuant })
+                        + this._calculateMatchLength(b, { pattern: nonQuantPattern, fn: this._addNonQuant });
+            
+            return bMax - aMax;
+        };
+
+        patternsWithMetas.sort(sortLogic);
+        patternsWithLiterals.sort(sortLogic);
+
+        const allSorted = patternsWithMetas.concat(patternsWithLiterals);
         console.log(allSorted);
 
         return allSorted;
     }
 
-    protected _calculateMinMatchLength(regexString: string) : number {
-        const countables = /\w+|(?=\\)[?+*$^{}()\[\]\\]/g
+    protected _calculateMatchLength(regexString: string, obj: LogicObj) : number {
+        const matchesList = regexString.matchAll(obj.pattern);
         let total = 0;
 
-        const includeInCount = regexString.match(countables);
-        console.log(includeInCount);
-
-        if (includeInCount !== null) {
-            for (const match of includeInCount) {
-                total += match.length;
-            }
+        if (matchesList === null)
             return total;
+
+        for (let matches of matchesList) {
+            total += obj.fn(matches);
         }
-
-        return 1;
-    }
-
-    // if * or + present theoretical limit is infinite -> sort these by minimum amount instead (10 -> inf, 2 -> inf, 10-28, 7)
-
-    protected _calculateMaxMatchLength(regexString: string) : number {
-        const quantPattern = /.\{(\d+),?\s?(\d+)?\}/;
-
-        let total = 0;
-
-        const finiteQuants = regexString.match(quantPattern);
-        if (finiteQuants !== null) {
-            const map: any = TemplateMapper.map(finiteQuants, '(minrange)(maxrange)');
-            total += (map.maxrange !== undefined) ? parseInt(map.maxrange) : parseInt(map.minrange);
-        }
-        
+        console.log(total, regexString);
         return total;
     }
 
-    protected _buildGroup(group: string[] | string) : string {
-        return (Array.isArray(group)) ? group.join(this._settings.separator || '|') : group;
+    protected _calculateMaxMatchLength(regexString: string) : number {
+        const quantPattern = /.\{(\d+),?\s?(\d+)?\}/g;
+        const nonQuantPattern = /(?!<=\{)\w+(?!\{)|(?=\\)[?+*$^{}()\[\]\\]/g;
+        let total = 0;
+
+        const matches = regexString.matchAll(quantPattern);
+        const includeInCount = regexString.matchAll(nonQuantPattern);
+
+        if (matches !== null) {
+            for (let match of matches) {
+                total += this._addQuant(match);
+            }
+        }
+
+        if (includeInCount !== null) {
+            for (const match of includeInCount) {
+                total += this._addNonQuant(match);
+            }
+        }
+
+        console.log(total, regexString);
+        return total;
     }
 
-    protected _substitutePlaceholder(group: string) : string {
-        const swap = (match: string, p1: string) => {
-            if (!this._placeholderData[p1]) throw new Error(`found undefined placeholder ${match} in regex data`);
-            return this._buildGroup(this._placeholderData[p1]);
-        };
+    protected _addQuant(match: RegExpMatchArray) : number {
+        const map: any = TemplateMapper.map(match, '(minrange)(maxrange)');
+        return (map.maxrange !== undefined) ? parseInt(map.maxrange) : parseInt(map.minrange);
+    }
 
-        return group.replace(/~~(\w+)~~/, swap);
+    protected _addNonQuant(match: RegExpMatchArray) : number {
+        const map: any = TemplateMapper.map(match, '');
+        return map.fullMatch.length;
     }
 
 }
